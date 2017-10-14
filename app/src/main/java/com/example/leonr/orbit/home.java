a754fdcc93261491ae5a75a3ea40c318324127c3
LeonSingleton <lsingleton1@sheffield.ac.uk>
2017-10-14 15:57:44 +0100

    latest

--- app/src/main/java/com/example/leonr/orbit/home.java
+++ app/src/main/java/com/example/leonr/orbit/home.java
@@ -1,13 +1,24 @@
 package com.example.leonr.orbit;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class home extends AppCompatActivity {
-
+    private FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(home.this,add_notification.class));
+        }
+        });
     }
 }
